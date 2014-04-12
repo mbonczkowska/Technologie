a0668bfc93da89913346193999068bf9f83f38c5
mbonczkowska <mbonczkowska@sigma.ug.edu.pl>
2014-04-13 00:01:26 +0200

    node

---
+++ app.js
@@ -0,0 +1,14 @@
+﻿var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+nicknames = []; // użytkownicy obecnie zalogowani
+
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+	app.use(express.static(__dirname + '/bower_components')); // deklaracja css/bootstrap
+	app.use(express.static(__dirname + '/pages'));
+});

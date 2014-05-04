27b9c81c76014ccbac8a9483c21ae5edbddc6950
mbonczkowska <mbonczkowska@sigma.ug.edu.pl>
2014-05-04 16:28:39 +0200

    logowanie

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 ﻿var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	nicknames = [];
 
 
 
@@ -11,4 +12,18 @@ app.get('/', function (req, res) {
     res.sendfile(__dirname + '/pages/index.html');
 	app.use(express.static(__dirname + '/bower_components')); // deklaracja css/bootstrap
 	app.use(express.static(__dirname + '/pages'));
+});
+
+io.sockets.on('connection',function(socket){
+	socket.on('new user',function(data,callback){
+		if(nicknames.indexOf(data) != -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			console.log(nicknames);
+			io.sockets.emit('usernames',nicknames);
+		}
+	});
 });
---
+++ pages/script/user.js
@@ -0,0 +1,34 @@
+﻿$(function(){
+	var socket = io.connect();
+	var $nickForm = $('#setNick');
+	var $nickError = $('#nickError');
+	var $nickBox = $('#nickname');
+	var $user = $('#user');
+	
+	$nickForm.submit(function(e){
+		e.preventDefault();
+		socket.emit('new user', $nickBox.val(), function(data){
+			if(data){			
+		
+			} else{
+				$nickError.html('Ten login już został użyty. Spróbuj ponownie');
+			}
+		});
+		$nickBox.val('');
+			
+	});
+	
+	socket.on('usernames',function(data){
+		$nickForm.hide();
+		$nickError.hide();
+		var last = data[data.length-1];
+		console.log(last);
+		$user.html(last);
+		/*var html = '';
+		for(i=0; i<data.length;i++){
+			html += data[i];
+		}
+		console.log(html);
+		$user.html(html);*/
+	});
+});
